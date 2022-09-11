9953bf5d863090d24baa6f4fdd45822f8d725d6a
Micael Jerry <hei.jerry.2@gmail.com>
2022-09-11 21:13:25 +0300

    fix: add method in eventparticipant repository

--- src/main/java/school/hei/haapi/repository/EventParticipantRepository.java
+++ src/main/java/school/hei/haapi/repository/EventParticipantRepository.java
@@ -1,9 +1,15 @@
 package school.hei.haapi.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.EventParticipant;
 
+import java.util.List;
+
 @Repository
 public interface EventParticipantRepository extends JpaRepository<EventParticipant,String> {
+    void deleteAllByEvent_Id(String eventId);
+
+    List<EventParticipant> getAllByStatus(school.hei.haapi.endpoint.rest.model.EventParticipant.StatusEnum status);
 }
