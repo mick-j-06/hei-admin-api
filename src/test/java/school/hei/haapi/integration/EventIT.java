35d359ffb37a009ac994d17cd851138c20d909aa
Micael Jerry <hei.jerry.2@gmail.com>
2022-09-11 17:43:48 +0300

    fix: test event 6/7

--- src/test/java/school/hei/haapi/integration/EventIT.java
+++ src/test/java/school/hei/haapi/integration/EventIT.java
@@ -12,14 +12,17 @@ import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import school.hei.haapi.SentryConf;
 import school.hei.haapi.endpoint.rest.api.EventApi;
+import school.hei.haapi.endpoint.rest.api.TeachingApi;
 import school.hei.haapi.endpoint.rest.client.ApiClient;
 import school.hei.haapi.endpoint.rest.client.ApiException;
+import school.hei.haapi.endpoint.rest.model.Course;
 import school.hei.haapi.endpoint.rest.model.Event;
 import school.hei.haapi.endpoint.rest.model.Place;
 import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
 import school.hei.haapi.integration.conf.AbstractContextInitializer;
 import school.hei.haapi.integration.conf.TestUtils;
 
+import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static school.hei.haapi.integration.conf.TestUtils.*;
@@ -47,8 +50,31 @@ public class EventIT {
         event.setRef("EVT21001");
         event.setStartingHours(Instant.parse("2022-09-11T12:00:00.00Z"));
         event.setEndingHours(Instant.parse("2022-09-11T14:00:00.00Z"));
-        event.setUserManagerId("manager1_id");
-        event.setPlaceId("place1_id");
+        event.setUserManagerId(MANAGER_ID);
+        event.setPlaceId(PLACE1_ID);
+        return event;
+    }
+
+    public static Event event2() {
+        Event event = new Event();
+        event.setId("event2_id");
+        event.setName("Name of event two");
+        event.setRef("EVT21002");
+        event.setStartingHours(Instant.parse("2022-10-11T12:00:00.00Z"));
+        event.setEndingHours(Instant.parse("2022-09-11T14:00:00.00Z"));
+        event.setUserManagerId(MANAGER_ID);
+        event.setPlaceId(PLACE1_ID);
+        return event;
+    }
+
+    public static Event someCreatableEvent() {
+        Event event = new Event();
+        event.setName("some name");
+        event.setRef("EVT-" + randomUUID());
+        event.setStartingHours(Instant.parse("2022-10-11T12:00:00.00Z"));
+        event.setEndingHours(Instant.parse("2022-09-11T14:00:00.00Z"));
+        event.setUserManagerId(TEACHER1_ID);
+        event.setPlaceId(PLACE2_ID);
         return event;
     }
 
@@ -72,6 +98,7 @@ public class EventIT {
         assertThrowsForbiddenException(() -> api.createOrUpdateEvents(List.of()));
     }
 
+    // TODO :   Fixed the time zone problem
     @Test
     void student_read_ok() throws ApiException {
         ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
@@ -82,6 +109,7 @@ public class EventIT {
 
         assertEquals(event1(), actual1);
         assertTrue(actualEvents.contains(event1()));
+        assertTrue(actualEvents.contains(event2()));
     }
 
     @Test
@@ -90,6 +118,52 @@ public class EventIT {
 
         EventApi api = new EventApi(student1Client);
         assertThrowsForbiddenException(() -> api.createOrUpdatePlaces(List.of()));
+    }
+
+    @Test
+    void teacher_write_ko() {
+        ApiClient teacher1Client = anApiClient(TEACHER1_TOKEN);
+
+        EventApi api = new EventApi(teacher1Client);
+        assertThrowsForbiddenException(() -> api.createOrUpdateEvents(List.of()));
+    }
+
+    @Test
+    void manager_write_create_ok() throws ApiException {
+        ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+        Event toCreate3 = someCreatableEvent();
+        Event toCreate4 = someCreatableEvent();
+
+        EventApi api = new EventApi(manager1Client);
+        List<Event> created = api.createOrUpdateEvents(List.of(toCreate3, toCreate4));
+
+        assertEquals(2, created.size());
+        assertTrue(isValidUUID(created.get(0).getId()));
+        toCreate3.setId(created.get(0).getId());
+        assertEquals(created.get(0), toCreate3);
+        //
+        assertTrue(isValidUUID(created.get(1).getId()));
+        toCreate4.setId(created.get(1).getId());
+        assertEquals(created.get(1), toCreate4);
+    }
+
+    @Test
+    void manager_write_update_ok() throws ApiException {
+        ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+        EventApi api = new EventApi(manager1Client);
+        List<Event> toUpdate = api.createOrUpdateEvents(List.of(
+                someCreatableEvent(),
+                someCreatableEvent()));
+        Event toUpdate0 = toUpdate.get(0);
+        toUpdate0.setName("A new name zero");
+        Event toUpdate1 = toUpdate.get(1);
+        toUpdate1.setName("A new name one");
+
+        List<Event> updated = api.createOrUpdateEvents(toUpdate);
+
+        assertEquals(2, updated.size());
+        assertTrue(updated.contains(toUpdate0));
+        assertTrue(updated.contains(toUpdate1));
     }
 
     static class ContextInitializer extends AbstractContextInitializer {
