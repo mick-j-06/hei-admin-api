ac8e58a858adbd4cbe6b487292a420ae997db937
Micael Jerry <hei.jerry.2@gmail.com>
2022-09-11 09:17:24 +0300

    fix: add pagination in swagger event

---
+++ src/main/java/school/hei/haapi/service/EventService.java
@@ -0,0 +1,10 @@
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EventService {
+
+}
