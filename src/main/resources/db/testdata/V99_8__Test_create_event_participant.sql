285b2f3b53df4f96b9bddb91523e6fe204836d71
Micael Jerry <hei.jerry.2@gmail.com>
2022-09-12 15:52:42 +0300

    fix: db for test eventparticipant

---
+++ src/main/resources/db/testdata/V99_8__Test_create_event_participant.sql
@@ -0,0 +1,9 @@
+insert into "event_participant"
+(id, status, user_participant, event)
+values
+    ('event_participant1_id','EXPECTED','student1_id','event1_id'),
+    ('event_participant2_id','EXPECTED','student2_id','event2_id'),
+    ('event_participant3_id','EXPECTED','student3_id','event1_id'),
+    ('event_participant4_id','EXPECTED','student1_id','event2_id'),
+    ('event_participant5_id','EXPECTED','student2_id','event1_id'),
+    ('event_participant6_id','EXPECTED','student3_id','event2_id');
