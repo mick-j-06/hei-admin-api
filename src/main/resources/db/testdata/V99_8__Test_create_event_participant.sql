d1db20683e0c76caf6bfce9b000a33be4866a4b2
Micael Jerry <hei.jerry.2@gmail.com>
2022-09-12 18:25:20 +0300

    fix : change insert event participant test

--- src/main/resources/db/testdata/V99_8__Test_create_event_participant.sql
+++ src/main/resources/db/testdata/V99_8__Test_create_event_participant.sql
@@ -3,6 +3,4 @@ insert into "event_participant"
 values
     ('event_participant1_id','EXPECTED','student1_id','event1_id'),
     ('event_participant2_id','EXPECTED','student2_id','event1_id'),
-    ('event_participant3_id','EXPECTED','student3_id','event1_id'),
-    ('event_participant4_id','EXPECTED','student1_id','event2_id'),
-    ('event_participant5_id','EXPECTED','student2_id','event2_id');
+    ('event_participant3_id','EXPECTED','student3_id','event1_id');
